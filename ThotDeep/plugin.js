79a647261136073c0ee12250611df7c58b66d0bf
RossiMesdo <117059595+RossiMesdo@users.noreply.github.com>
2026-05-26 07:15:55 +0700

    Create plugin.js

---
+++ ThotDeep/plugin.js
@@ -0,0 +1,11 @@
+{
+  "packageName": "dev.lord.thotdeep",
+  "name": "ThotDeep",
+  "version": 1,
+  "baseUrl": "https://thotdeep.com",
+  "description": "ThotDeep - Celebrity and influencer leaks",
+  "authors": ["Lord"],
+  "languages": ["en"],
+  "categories": ["Movie"],
+  "isAdult": true
+}
